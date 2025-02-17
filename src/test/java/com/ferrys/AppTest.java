79af115c13d7ece631458a3c41af507715995933
ferrys-appen <fsutanto@appen.com>
2025-02-17 13:56:57 +0700

    change message for debug purpose

--- src/test/java/com/ferrys/AppTest.java
+++ src/test/java/com/ferrys/AppTest.java
@@ -29,6 +29,6 @@ public class AppTest
      * Rigourous Test :-)
      */
     public void testApp() {
-        assertEquals("Hello, World!", App.greet());
+        assertEquals("Hello, World!1", App.greet());
     }
 }
